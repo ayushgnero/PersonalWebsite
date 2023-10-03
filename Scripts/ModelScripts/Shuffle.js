846852e26a02cbb5452c0e82cfdf3bd7d3743b93
ayushgnero <ayushgautam171099@gmail.com>
2023-10-03 10:35:09 +0530

    temp commit

---
+++ Scripts/ModelScripts/Shuffle.js
@@ -0,0 +1,49 @@
+const random_char = () => {
+    const possible = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" +
+          "0123456789" +
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+          "abcdefghijklmnopqrstuvwxyz";
+    return possible.charAt(Math.floor(Math.random() * possible.length));
+  };
+  
+  const mask = (chars, progress) => {
+    const masked = [];
+  
+    for (let i = 0; i < chars.length; i++) {
+      const position = (i + 1) / chars.length;
+      if (position > progress) {
+        masked.push(random_char());
+      } else {
+        masked.push(chars[i]);
+      }
+    }
+  
+    return masked.join('');
+  };
+  
+  const shuffle = el => {
+    const chars = el.textContent.split('');
+  
+    const params = {
+      progress: 0
+    };
+  
+    const a = anime({
+      targets: params,
+      progress: 1,
+      delay: 1000,
+      duration: 1000,
+      easing: 'easeInQuad',
+      update: () => {
+        el.textContent = mask(chars, params.progress);
+      },
+      complete: () => {
+        el.classList.add('completed');
+      }
+    });
+
+  };
+  
+  for (const el of document.querySelectorAll('.shuffle')) {
+    shuffle(el);
+  }
